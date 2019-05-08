30bf7f0470e05b1af96a81077373815b8a16116c
Achede_HD <hernan.david.hd@gmail.com>
2019-05-07 21:56:38 -0300

    Update lvm.sh

--- lvm.sh
+++ lvm.sh
@@ -2,14 +2,21 @@
 lsblk    # listar bloques
 df -h    # ver espacio libre 
 
+lsblk    # listar bloques
+df -h    # ver espacio libre 
 gdisk /dev/sdc    #PARTICIONAR DISCO > n > enter, enter, p
-	pv -> pvcreate /dev/sdd1 /dev/sde1 /dev/sdf1
-	vg -> vgcreate GRUPO /dev/sdd1 /dev/sde1 /dev/sdf1
-	lv -> lvcreate -l +100%FREE GRUPO /dev/sdd1 /dev/sde1 /dev/sdf1 -n VOLUMEN
-
-
-mkfs.xfs /dev/GRUPO/VOLUMEN #FORMATEAR volumen 
-mkdir /mnt/CARPETA2 # Crear carpeta 
-mount /dev/GRUPO/VOLUMEN /mnt/CARPETA2 #MONTAR
 
 free -h  # ver memoria
+
+MONTAR
+mount /dev/GRUPO/VOLUMEN /mnt/CARPETA2 #MONTAR
+mkdir /mnt/CARPETA2 # Crear carpeta 
+
+FORMATEAR EN ext4
+mkfs.xfs /dev/GRUPO/VOLUMEN #FORMATEAR volumen 
+		lv -> lvcreate -l +100%FREE GRUPO /dev/sdd1 /dev/sde1 /dev/sdf1 -n VOLUMEN
+		vg -> vgcreate GRUPO /dev/sdd1 /dev/sde1 /dev/sdf1
+		pv -> pvcreate /dev/sdd1 /dev/sde1 /dev/sdf1
+FORMATEAR EN LVM
+PARTICIONAR
+gdisk /dev/sdb
